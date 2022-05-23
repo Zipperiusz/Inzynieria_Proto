127be9b8c582f1a75847f9d430994ac4cc5ab7f2
Zipperiusz <Zipperiusz0@gmail.com>
2022-05-23 14:05:53 +0200

    project

---
+++ PolTrain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PolTrain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
